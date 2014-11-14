f512968170ac8d555a214282042c739d049c1bf9
hunterdavidsmith <huntersmith200@gmail.com>
2014-11-14 09:13:53 -0800

    commenting

--- model/Database.php
+++ model/Database.php
@@ -6,5 +6,14 @@ class Database{
 	 private $pasword;
 	 private $database;
 }
-//what we want to do is make variables and clases in the iles
+//what we want to do is make variables and clases in the variables 
+
+//  we use clases because classes are isolated code from everything else witch makes it easyer to move them arround 
+// classes are a way to view your code in a more intuitive, real-world way. (You package the data and all possible operations on that data together) 
+
+
 // within the personal object we will be able to store all our information  in our variable
+
+// and the reason we refactorf our code and improve the way it looks and make it easyer to maintain
+
+//we erased our old database because we dont need it anymore we have stored all of our files in cinfig.php so we dont need our database 
