03126f61d016bbf5236a6059704f4324a92c811f
hunterdavidsmith <huntersmith200@gmail.com>
2014-11-19 08:45:32 -0800

    finsihed video 32

--- controler/create-post.php
+++ controler/create-post.php
@@ -6,12 +6,10 @@
    // once we know what dirrectory we are in we can get out of it and require anohte file
     require_once(__DIR__ . "/../model/config.php");
 //the reason we have our mysql is so we can have a connection 
-
-     $connection = new mysqli($host, $username, $pasword, $database);
      
      $title = filter_input(INPUT_POST,"title", FILTER_SANITIZE_STRING);
      $post = filter_input(INPUT_POST,"post", FILTER_SANITIZE_STRING) ;
-//well echo this out to then see if we have filterd our code well
+ //well echo this out to then see if we have filterd our code well
     
     //we need to make our query and use it to post our blog post on out post eres
   
@@ -27,10 +25,7 @@
 
 
 
-    $connection->close();
-    
-     //we neeed to make sure we close our $connection variable
-
+  
        
      
      //this information will be sent to the database later 
