b717d7fe4d1d5b5996bb140556f55ef7a43c3e4f
hunterdavidsmith <huntersmith200@gmail.com>
2014-12-02 08:44:25 -0800

    finsihing video 37 making a sing up thingy

---
+++ veiw/register-form.php
@@ -0,0 +1,21 @@
+<form>
+     <div>
+          <label for="email">Email: </label>
+          <input type="text" name="email" />
+     </div>
+<!-- this code is so people can log into our blog post form -->
+    <div>
+    	  <label for="username">username:</label>
+          <input type="text" name="username"> />
+    </div>
+
+    <div>
+    	 <label for="password">password:</label>
+    	 <input type="password" name="password" />
+    </div>
+     
+     <div>
+     	  <button type="submit">submit</button>
+     </div>
+</form>
+<!-- we need to tell the code to do something with this code and allow peple to sing up for the blog post -->
